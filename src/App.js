6a8c7ffa2fe0c6cc1658150b16ceee2589207db4
Charline Ramelet <charline.ramelet@gmail.com>
2023-02-05 17:34:56 +0100

    creating modal library

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './app.scss';
+import LightModal from './lib/light-modal/light-modal';
+import SimpleButton from './components/button/button';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    const [modalVisible, setModalVisible] = useState(false);
+
+    const toogleModal = (e) => {
+        e.preventDefault();
+        setModalVisible(!modalVisible);
+    };
+
+    return (
+        <div className='test-container'>
+            <LightModal onClick={toogleModal} modalVisible={modalVisible} text={"Employee created!"} />
+            <SimpleButton onClick={toogleModal} />
+        </div>
+    )
 }
-
-export default App;
---
+++ src/lib/light-modal/light-modal.js
@@ -0,0 +1,28 @@
+import "./light-modal.scss";
+import CloseRoundedIcon from "@mui/icons-material/CloseRounded";
+
+function LightModal({ onClick, modalVisible, text }) {
+    if (modalVisible) {
+        return (
+            <>
+                <div className="modal-container">
+                    <div className="modal-backdrop"></div>
+                    <div className="modal-wrapper">
+                        <div className="modal-content">
+                            <div className="modal-body">
+                                <span className="modal-text">{text}</span>
+                            </div>
+                            <CloseRoundedIcon
+                                className="close-button"
+                                type="button"
+                                aria-label="Close"
+                                onClick={onClick}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    };
+}
+export default LightModal;
